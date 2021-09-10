3be4a1b94867d5d2c8b03d29f703e555ba7d7f2f
deltacastro <abel.castro9111@gmail.com>
2021-09-10 20:38:39 +0000

    Actualizacion

--- resources/views/layouts/sidebar/index.blade.php
+++ resources/views/layouts/sidebar/index.blade.php
@@ -1,5 +1,5 @@
-<div class="d-flex flex-column flex-shrink-0 py-3 text-white bg-navy-blue" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+<div class="d-flex flex-column flex-shrink-0 py-3 text-gray-800 bg-white shadow" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-gray-800 text-decoration-none fs-4">
         <svg class="bi me-2" width="40" height="32">
             <use xlink:href="#bootstrap"></use>
         </svg>
@@ -8,7 +8,7 @@
     <hr>
     <ul class="nav nav-pills flex-column mb-auto fw-bold">
         <li class="nav-item">
-            <a href="{{ route('dashboard') }}" class="nav-link text-white {{ (request()->is('dashboard')) ? 'active disabled' : '' }}">
+            <a href="{{ route('dashboard') }}" class="nav-link text-gray-800 {{ (request()->is('dashboard')) ? 'active disabled' : '' }}">
                 <i class="bi bi-house-door me-2"></i>
                 Inicio
             </a>
@@ -17,7 +17,7 @@
             <hr class="dropdown-divider">
         </li>
         <li class="nav-item">
-            <a href="{{ route('admin.user.index') }}" class="nav-link text-white {{ (request()->is('admin/usuarios')) ? 'active disabled' : '' }}">
+            <a href="{{ route('admin.user.index') }}" class="nav-link text-gray-800 {{ (request()->is('admin/usuarios')) ? 'active disabled' : '' }}">
                 <i class="bi bi-people me-2"></i>
                 Admin Usuarios
             </a>
