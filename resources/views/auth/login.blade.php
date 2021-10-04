e2b8c51adf8b20661599fb7c82239e11d8a5da0a
deltacastro <abel.castro9111@gmail.com>
2021-10-04 03:42:37 +0000

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,56 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.main')
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('content')
+    <div class="d-flex justify-content-center align-items-center h-100 bg-gray-200">
+        <div class="card shadow-lg col-11 col-md-8 col-lg-6 col-xl-4">
+            <form method="POST" action="{{ route('login') }}" class="card-body text-gray-700 fw-bold">
+                <div class="col mb-4 d-flex flex-wrap justify-content-center">
+                    <img class="img-fluid px-5 my-3 col-12" src="{{ asset('images/logo.png') }}" alt="" style="width: 60%;">
+                    <h4 class="fw-bold text-center text-gray-800 col-12">Iniciar Sesión</h4>
+                </div>
+                @csrf
+                <!-- Email Address -->
+                <x-form.float.input type="email" class="text-gray-800 fw-bold" label="Correo" id="correo"
+                    name="email" error-validator="email" value="{{ old('email') }}"/>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                <!-- Password -->
+                <x-form.float.input type="password" class="text-gray-800 fw-bold" label="Contraseña" id="password"
+                    name="password" error-validator="password"/>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                <!-- Remember Me -->
+                <div class="d-block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                        <span class="ml-2 text-sm text-gray-600">Recuerdame</span>
+                    </label>
+                </div>
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                <div class="d-flex align-items-center justify-content-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                            Recuperar contraseña
+                        </a>
+                    @endif
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+                    <x-button class="ms-3">
+                        Iniciar sesión
+                    </x-button>
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+            </form>
+        </div>
+    </div>
+@endsection
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@section('javascript')
+    <script src="{{ asset('js/select2.js') }}"></script>
+    <script>
+        $(function(){
+            // $.fn.select2.defaults.set( {"theme", "bootstrap-5", selectionCssClass: "select2--large",} );
+            $('#email').select2({
+                theme: "bootstrap-5",
+                containerCssClass: "select2--large", // For Select2 v4.0
+            });
+        })
+    </script>
+@endsection
