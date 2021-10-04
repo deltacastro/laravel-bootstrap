d52055d594c54ee887b5fa0edd66f763bb1d1426
deltacastro <abel.castro9111@gmail.com>
2021-10-04 05:45:08 +0000

    Se agrega gate para los usuarios super admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,8 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        // Gate::before(function ($user, $ability) {
-        //     return $user->hasRole('super admin') ? true : null;
-        // });
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('super admin') ? true : null;
+        });
     }
 }
