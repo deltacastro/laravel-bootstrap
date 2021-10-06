b24dd0042eb4479bbeb2d8251985b0e93bbe5b68
deltacastro <abel.castro9111@gmail.com>
2021-10-06 15:13:08 -0500

    Actualizacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.main')
 
-
 @section('content')
     <div class="d-flex h-100 align-items-center col-12 justify-content-center">
         <img class="img-fluid col-10 col-lg-7 col-xll-5" src="{{ asset('images/logo.png') }}" alt="">
